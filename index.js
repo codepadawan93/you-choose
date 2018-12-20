bbebdac6a8942f2c4e8531fde17fa2c1a6a913a7
codepadawan93 <kovaroberik@gmail.com>
2018-12-20 22:39:54 +0200

    begun building restful service

--- index.js
+++ index.js
@@ -15,16 +15,85 @@ server.use(bodyParser.json());
 // Serve static assets from ./client
 server.use(express.static(path.join(__dirname, "client/build")));
 
-// Respond to API calls
-server.get("/api/:route", (req, res) => {
-  console.log(req.params.route);
-  console.log(req.body);
-  res.json({ message: "Hello World", success: true });
+// Load models
+const { User, List, Role, Movie } = require("./models/index");
+
+// Respond to API GET calls - simple router
+server.get(["/api/:route/", "/api/:route/:id"], (req, res) => {
+  // We need the route and the id
+  const { route, id } = req.params;
+  switch (route) {
+    case "users":
+      if (id) {
+        User.findAll({ where: { user_id: id } }).then(data => {
+          res.json({ data, success: true });
+        });
+      } else {
+        User.findAll().then(data => {
+          res.json({ data, success: true });
+        });
+      }
+      break;
+    case "movie":
+      if (id) {
+        Movie.findAll({ where: { movie_id: id } }).then(data => {
+          res.json({ data, success: true });
+        });
+      } else {
+        Movie.findAll().then(data => {
+          res.json({ data, success: true });
+        });
+      }
+      break;
+    case "list":
+      if (id) {
+        List.findAll({ where: { list_id: id } }).then(data => {
+          res.json({ data, success: true });
+        });
+      } else {
+        List.findAll().then(data => {
+          res.json({ data, success: true });
+        });
+      }
+      break;
+  }
+});
+
+// POST calls
+// Respond to API GET calls - simple router
+server.post(["/api/:route/", "/api/:route/:id"], (req, res) => {
+  const { route, id } = req.params;
+
+  switch (route) {
+    case "users":
+      // TODO:: we should validate really
+      if (id) {
+        User.findOne(id).then(user => {
+          user
+            .update(req.body)
+            .then(data => res.json({ data: [user], success: true }));
+        });
+      } else {
+        const user = User.build(req.body);
+        user.save().then(data => res.json({ data: [user], success: true }));
+      }
+      break;
+    case "movie":
+      if (id) {
+      } else {
+      }
+      break;
+    case "list":
+      if (id) {
+      } else {
+      }
+      break;
+  }
 });
 
 // Reroute everything else to React app in client
 server.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 // Start the server
